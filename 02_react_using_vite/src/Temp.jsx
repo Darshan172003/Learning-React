db8b55311a551aa1b59869d522a8eaae2af03bce
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-07-23 20:45:37 +0530

    Creating a react project using vite "npm create vite@latest"

---
+++ 02_react_using_vite/src/Temp.jsx
@@ -0,0 +1,12 @@
+function Temp() {
+    return (
+        <div>
+            <h2>Creating a react project using vite</h2>
+            <h2>IMP!!!! :- If you get an error while running react project so edit a package.json  &quot;dev&quot;: &quot;vite --host&quot; to run the project on local host </h2>
+            {/* quote is used to escape the double quotes*/}
+            {/* In react we use a curly braces for comments */}
+        </div>
+    )
+}
+
+export default Temp
