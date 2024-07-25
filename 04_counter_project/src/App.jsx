e82cbdc4a405fd7d4c555526d7546cf64e821cf4
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-07-25 16:17:00 +0530

    learning hooks using counter project

---
+++ 04_counter_project/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setcounter] = useState(0)
+
+  // let counter =  0
+
+  const addValue = () => {
+      if (counter < 20){
+        setcounter(counter + 1)
+      }
+  }
+  const removeValue = () => {
+    if (counter > 0 ){
+      setcounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Learning React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
