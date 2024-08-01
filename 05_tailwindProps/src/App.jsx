096ff1feb380b61a8bb325fa00808d40af59f9c5
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-08-01 18:57:20 +0530

    learning props

---
+++ 05_tailwindProps/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-400 text-black rounded-xl p-1 font-serif mb-2'>Tailwind and Props</h1>
+      <Card />
+      <Card />
+    </>
+  )
+}
+
+export default App
