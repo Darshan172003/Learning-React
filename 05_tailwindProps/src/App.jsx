6400e25ad1f5053b310c01626d938f52ca8f7b21
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-08-03 11:14:24 +0530

    props in react

--- 05_tailwindProps/src/App.jsx
+++ 05_tailwindProps/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black rounded-xl p-1 font-serif mb-2'>Tailwind and Props</h1>
-      <Card />
-      <Card />
+      <Card Username="User1" btnText="Show Profile"/>
+      <Card Username="User2" btnText="visit Profile"/>
+      
     </>
   )
 }
