6cf7dfc68c8b3eb8c3cc065e7a4b29da20012e36
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-08-05 22:37:46 +0530

    Password Generator Project

---
+++ 07_passwordGenerator/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex flex-wrap w-full h-full px-5 py-20 justify-center shadow-xl rounded-2xl'>
+        <h3>Password Generator</h3><br/>
+        <div>
+          <input type="text" placeholder='Password'/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
