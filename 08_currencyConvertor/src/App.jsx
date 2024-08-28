ffa6a90ae3abbef26fe963d1bae32e8317a5a007
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-08-28 19:39:23 +0530

    CurrenyConvertor using custom hook

---
+++ 08_currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className=''>
+      <h1 className='text-3xl bg-orange-500 my-4 py-4 text-center rounded-xl w-1/2'>Currency Convertor</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
