fd9139c3a023a56903b4c608df643d5495a15eae
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2025-03-12 11:21:40 +0530

    add UserContext and Login components for user authentication

--- 09_useContext/src/App.jsx
+++ 09_useContext/src/App.jsx
@@ -1,10 +1,12 @@
+import UserContextProvider from '../context/UserContextProvider'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1 className="text-5xl font-semibold text-violet-600 text-center">useContext</h1>
-    </>
+    </UserContextProvider>
+
   )
 }
 
