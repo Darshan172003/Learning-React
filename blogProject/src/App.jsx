327ec257357f54f6d9c07a439c27d9c611f6f4ee
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2025-03-16 20:09:25 +0530

    add: implement user authentication with Appwrite in App component

--- blogProject/src/App.jsx
+++ blogProject/src/App.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './redux/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  
-  const env = import.meta.env.VITE_APPWRITE_URL
 
-  return (
-    <div>
-      <h1>Blog Project {env}</h1>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
     </div>
-  )
+  ) : <div>Loading...</div>
+
+
 }
 
 export default App
